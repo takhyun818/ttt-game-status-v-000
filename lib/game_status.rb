bfc51fec84886ace59695068c7950f762c7fb35e
Tak H Kim <takhyun818@gmail.com>
2018-08-12 23:41:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,6 +24,7 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    position_1 == "X" && position_2 == "X" && position_3 == "X"
+    position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+    position_1 == "O" && position_2 == "O" && position_3 == "O"
   end
 end
